ec758f2e8663ab650777b7e0a24db36d07092e85
Erick Araújo <araujoerick.dev@gmail.com>
2023-12-21 21:51:51 -0300

    :sparkles: feat: adiciona JS para mostrar/ocultar FAQ

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Ativar Links do Menu
+
 // Pego o array-like de links
 const links = document.querySelectorAll(".header-menu a");
 
@@ -18,3 +20,58 @@ function ativarLink(link) {
 
 // Passo por cada item link com um loop
 links.forEach(ativarLink);
+
+// Ativar Ítens do Orçamento
+
+// O JS já vem com uma classe que pega os parâmetros de uma url
+// Para ativar uma classe é necessário colocar o new na frente
+// const parametros = location.search;
+const parametros = new URLSearchParams(location.search);
+
+// Lista os parametros com o loop
+// Pega o elemento input pelo id, que tem o mesmo nome do parâmetro
+// Se elemento existe, faça
+// Adiciona o checked nos elementos capturados
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+  // console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+// Pega a node list de perguntas com querySelectAll
+// Lista cada botão/pergunta com o loop forEach
+// Adiciona o evento de click e passa a função ativarPergunta que vai ocorrer ao clique
+// Dentro do evento tem sempre acesso ao parâmetro de evento, o event
+// E pegar o elemento/pergunta clicada com o currentTarget
+// Pegar o atributo do aria-controls que é o mesmo que o id da resposta
+// Seleciona resposta pelo id
+// Adiciona a class ativa na resposta que foi clicada com classList e toggle
+// É preciso mudar o aria-expanded também para true ou false
+// Para isso, verifica se a resposta contain a class ativa
+// Isso retorna um boolean, se sim true, senão false
+// Setamos o valor com o setAttribute que transforma o boolean em string automaticamente
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
